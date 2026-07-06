8403f090c917688355bdf41181d0eae30ba81bd8
sajjad-ansareee <sajjadansareee@gmail.com>
2026-07-06 16:50:30 +0500

    start with C code

--- board_cell.cpp
+++
@@ -1,5 +0,0 @@
-#include "board_cell.h"
-
-/* -1 indicates the cell contains no ship part */
-Board_Cell::Board_Cell()
-    : ship_number(-1), ship_part_number(-1), has_ship_part(false) {}
--- ships.h
+++
@@ -1,6 +0,0 @@
-class Ships {
-public:
-  void initialize() {}
-
-private:
-};
